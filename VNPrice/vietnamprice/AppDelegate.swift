259704ded567c60148518e4b28ec7b81a528077f
VanQuang <khang.quang113@gmail.com>
2017-07-29 21:30:20 +0700

    Quang add topic

--- VNPrice/vietnamprice/AppDelegate.swift
+++ VNPrice/vietnamprice/AppDelegate.swift
@@ -34,6 +34,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any]) {
         print(userInfo)
     }
+    
+    func application(application: UIApplication, didRegisterUserNotificationSettings notificationSettings: UIUserNotificationSettings) {
+        print("Hooray! I'm registered!")
+        FIRMessaging.messaging().subscribeToTopic("/topics/swift_fans")
+    }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
