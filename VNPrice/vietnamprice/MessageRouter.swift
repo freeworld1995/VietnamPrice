11585c57d100db251feb211986414e640bb88174
VanQuang <khang.quang113@gmail.com>
2017-07-29 14:15:36 +0700

    MESSAGE ROUTER

--- VNPrice/vietnamprice/MessageRouter.swift
+++ VNPrice/vietnamprice/MessageRouter.swift
@@ -10,7 +10,49 @@ import UIKit
 import Foundation
 import Alamofire
 enum MessageRouter: URLRequestConvertible {
-    static let baseURL = "http://103.237.147.10:8080/"
+    static let baseURL = "http://103.237.147.10:8080/api/"
     
-    case loginUserByEmail(String, String)
+    case getMessage([String: Any])
+    func asURLRequest() throws -> URLRequest {
+        var method: HTTPMethod {
+            return .post
+
+        }
+        let url: URL = {
+            let relativePath = "messages"
+            var url = URL(string: MessageRouter.baseURL)!
+            url.appendPathComponent(relativePath)
+            return url
+        }()
+        
+        let params: Parameters? = {
+            return nil
+        }()
+        
+        
+        let httpBody: Data? = {
+            switch self {
+            case .getMessage(let dict):
+                return try! JSONSerialization.data(withJSONObject: dict, options: .prettyPrinted)
+            }
+        }()
+        
+        
+        let cstorage = HTTPCookieStorage.shared
+        if let cookies = cstorage.cookies(for: url) {
+            for cookie in cookies {
+                cstorage.deleteCookie(cookie)
+            }
+        }
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.cachePolicy = .reloadIgnoringLocalCacheData
+        urlRequest.httpMethod = method.rawValue
+        urlRequest.httpBody = httpBody
+
+        let encoding = URLEncoding.default
+        
+        return try! encoding.encode(urlRequest, with: params)
+            
+    }
 }
