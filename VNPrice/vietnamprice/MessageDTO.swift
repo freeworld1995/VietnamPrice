f4998ebc50d8b3476d6376f880b999a219abca41
VanQuang <khang.quang113@gmail.com>
2017-07-29 14:03:09 +0700

    Quang add MessageDTO

---
+++ VNPrice/vietnamprice/MessageDTO.swift
@@ -0,0 +1,35 @@
+//
+//  MessageDTO.swift
+//  VietNamePrice
+//
+//  Created by VanQuang on 7/29/17.
+//  Copyright © 2017 VanQuang. All rights reserved.
+//
+
+import UIKit
+import ObjectMapper
+class MessageDTO: Mappable {
+    var id: Int?
+    var description: String?
+    var createdDate: String?
+    var userId: String?
+    
+    
+    required init?(map: Map) {
+    }
+    
+    func mapping(map: Map) {
+        id <- map["id"]
+        description <- map["description"]
+        createdDate <- map["createdDate"]
+        userId <- map["userId"]
+    }
+    
+    init() {
+        id = 0
+        description = ""
+        createdDate = ""
+        userId = ""
+    }
+
+}
---
+++ VNPrice/vietnamprice/MessageRouter.swift
@@ -0,0 +1,13 @@
+//
+//  MessageRouter.swift
+//  VietNamePrice
+//
+//  Created by VanQuang on 7/29/17.
+//  Copyright © 2017 VanQuang. All rights reserved.
+//
+
+import UIKit
+
+class MessageRouter: NSObject {
+
+}
